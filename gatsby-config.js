6b75606281f6ab4833df9baba32292d047e4007d
MacKenzie Kerwin <kerwin.m@husky.neu.edu>
2022-03-07 15:44:31 -0500

    fix dotenv import and plugin options

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `new`,
@@ -12,6 +16,9 @@ module.exports = {
         repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
         accessToken: process.env.PRISMIC_ACCESS_TOKEN,
         customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        schemas: {
+          my_deleted_schema: {},
+        },
       },
     },
   ],
