2b153c6401e0f1911b2f268001bb1d78f02b9cb2
gabbybruck <gabby.bruck@gmail.com>
2022-04-07 23:00:05 -0400

    sections

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,16 @@
 import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 import Layout from "../components/Layout";
+import {
+  ButtonText,
+  Container,
+  H1,
+  H2,
+  H3,
+  P,
+  PrimaryButton,
+  YELLOW,
+} from "../styles/styles";
 
 const About = () => {
   const aboutQuery = useStaticQuery(graphql`
@@ -45,12 +55,24 @@ const About = () => {
 
   return (
     <Layout>
-      <h1>{mainHeading.text}</h1>
+      <H1>{mainHeading.text}</H1>
       {sections.map((section) => {
-        return section.section_header.text; //TODO;
+        return (
+          <div>
+            <img
+              src={section.image.url}
+              alt={section.section_header.text}
+              width="30%"
+            />
+            <H3>{section.section_header.text}</H3>
+            <P>{section.section_text.text}</P>
+          </div>
+        );
       })}
-      <h1>{calloutHeading.text}</h1>
-      <h1>{calloutLabel.text}</h1>
+      <H2>{calloutHeading.text}</H2>
+      <PrimaryButton>
+        <ButtonText>{calloutLabel.text}</ButtonText>
+      </PrimaryButton>
     </Layout>
   );
 };
