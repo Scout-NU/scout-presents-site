11d953760670826e1fed1354523c45d2d6660cc8
MacKenzie Kerwin <kerwin.m@husky.neu.edu>
2022-05-14 20:57:30 -0400

    update padding

--- src/components/ProjectPreview/styled.js
+++ src/components/ProjectPreview/styled.js
@@ -17,19 +17,18 @@ export const FlexContainer = styled(MarginContainer)`
 
 export const Overview = styled.div`
   flex: 50%;
-  padding-bottom: 1rem;
+  padding: 3rem 0;
   ${min.desktop} {
     border-right: 0.25px solid ${CREAM};
-    padding: 1rem;
-    padding-left: 0;
+    padding: 6rem 1rem 3rem 0;
   }
 `;
 
 export const Content = styled.div`
   flex: 50%;
+  padding-bottom: 3rem;
   ${min.desktop} {
     border-left: 0.25px solid ${CREAM};
-    padding: 1rem;
-    padding-right: 0;
+    padding: 10rem 0 3rem 1rem;
   }
 `;
