3f4e29a2d2cd5d21c12d6948c21254f49bee9645
MacKenzie Kerwin <kerwin.m@husky.neu.edu>
2022-05-21 18:16:30 -0400

    sprinkles and padding

--- src/components/Callout/styled.js
+++ src/components/Callout/styled.js
@@ -1,6 +1,7 @@
-import styled from "styled-components";
-import { PINK, BLACK } from "../../styles/styles";
-import { min } from "../../styles/breakpoints";
+import styled from 'styled-components';
+import { PINK, BLACK } from '../../styles/styles';
+import { min } from '../../styles/breakpoints';
+import { css } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
@@ -9,7 +10,9 @@ export const Container = styled.div`
   text-align: center;
   white-space: nowrap;
   color: ${BLACK};
-  padding-bottom: 5%;
+  position: relative;
+  overflow-x: clip;
+  padding: 3rem 0;
 `;
 
 export const LargeButton = styled.div`
@@ -22,5 +25,44 @@ export const LargeButton = styled.div`
   }
   ${min.desktop} {
     width: 33.33%;
+  }
+`;
+
+export const sprinkleStyles = css`
+  position: absolute;
+  z-index: 1;
+`;
+
+export const LeftSprinkle = styled.img`
+  ${sprinkleStyles};
+  width: 248px;
+  top: -4rem;
+  left: -6rem;
+  ${min.tablet} {
+    width: 405px;
+    top: -9rem;
+    left: -7rem;
+  }
+  ${min.desktop} {
+    width: 374px;
+    top: -8rem;
+    left: -6rem;
+  }
+`;
+
+export const RightSprinkle = styled.img`
+  ${sprinkleStyles};
+  width: 129px;
+  top: -3rem;
+  right: -3rem;
+  ${min.tablet} {
+    width: 214px;
+    top: -6rem;
+    right: -3rem;
+  }
+  ${min.desktop} {
+    width: 198px;
+    top: -5rem;
+    right: 5rem;
   }
 `;
