ff611544e950e23fe939159c6c39085ab5b8c657
MacKenzie Kerwin <kerwin.m@husky.neu.edu>
2022-05-22 13:11:24 -0400

    attempt 2

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -2,10 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import { BLACK, CREAM, CursorStyles, PointerStyles } from '../../styles/styles';
 
 export const GlobalStyle = createGlobalStyle`
-  html, body {
-    position: relative;
-    overflow-x: clip;
-  }
   body {
     color: ${CREAM};
     background-color: ${BLACK};
